dd9e8a544cbe2bb82e04a29d419d4cf1e4170e8c
hoanhua14 <huasohoan@gmail.com>
2025-05-15 22:30:15 -0700

    customers table created

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,20 @@
 CREATE TABLE Products (
     name TEXT NOT NULL,
-    id TEXT PRIMARY KEY NOT NULL,
+    id Serial PRIMARY KEY NOT NULL,
     price INT NOT NULL,
     portion TEXT NOT NULL
-)
+);
+
+CREATE TABLE Customers (
+    name TEXT NOT NULL,
+    phone VARCHAR(15) PRIMARY KEY NOT NULL,
+    address TEXT NOT NULL,
+    delivery TEXT NOT NULL,
+    note TEXT NOT NULL,
+    payment_method TEXT CHECK (payment_method IN ('cash', 'wire')),
+    wire_number TEXT,
+    CHECK(
+        (payment_method='wire' AND wire_number is NOT NULL)
+        or (payment_method='cash' AND wire_number is NULL)
+    )
+);
